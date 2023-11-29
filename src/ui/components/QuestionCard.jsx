69454356af9c68dd1c6d362b0d82ac05a5e1de0e
“DesireeEspinosa” <desireeespinosacontreras@gmail.com>
2023-11-29 04:46:58 -0600

    Help Page complete

--- src/ui/components/QuestionCard.jsx
+++ src/ui/components/QuestionCard.jsx
@@ -1,20 +1,21 @@
 import React from "react";
-import { Box, Text, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from "@chakra-ui/react";
+import { Box, Text, Image, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from "@chakra-ui/react";
 import "@fontsource/mitr/400.css";
 
-const QuestionCard = ({ titleText, children }) => {
+const QuestionCard = ({ titleText, children, imageUrl }) => {
   return (
     <AccordionItem>
       <h2>
-        <AccordionButton>
+        <AccordionButton _expanded={{ bg: "brand.102", color: 'white' }}>
           <Box flex="1" textAlign="left">
-            <Text fontSize="lg">{titleText}</Text>
+            <Text fontSize="xl">{titleText}</Text>
           </Box>
           <AccordionIcon />
         </AccordionButton>
       </h2>
       <AccordionPanel pb={4}>
         {children}
+        {imageUrl && <Image src={imageUrl} alt="Reference map"/>}
       </AccordionPanel>
     </AccordionItem>
   );
